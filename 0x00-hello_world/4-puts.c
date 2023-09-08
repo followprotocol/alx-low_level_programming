dd57a3d20f56a6da79a096726325be5160e09a61
followprotocol <followprotcol@tuta.io>
2023-09-07 17:01:20 -0700

    4-puts fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - a program that prints a statement using the puts function
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
