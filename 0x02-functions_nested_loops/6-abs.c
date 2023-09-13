a373803b2583614314e68f2a0376abb47d306102
followprotocol <followprotcol@tuta.io>
2023-09-13 06:46:39 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of an integer
+ * @c: parameter passed to the function
+ * Return: returns the absolute value (success)
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
