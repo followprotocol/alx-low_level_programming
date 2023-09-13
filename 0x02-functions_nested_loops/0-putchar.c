02f22ba57960dbd9d3c0aac620c06be43e220242
followprotocol <followprotcol@tuta.io>
2023-09-13 06:40:27 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar as an output.
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0); /* Indicates successful execution */
+}
