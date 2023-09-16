a9d4b47e2fd8c44df0333996771258eee10c9242
followprotocol <followprotcol@tuta.io>
2023-09-15 19:48:41 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: an arguement passed to the function
+ * @b: an argument passed to the function
+ * Return: mul (result)
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul); /* return result */
+}
