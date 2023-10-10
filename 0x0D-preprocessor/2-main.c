478623ef3d8317b9d85be59b95d3304e4957c117
followprotocol <followprotcol@tuta.io>
2023-10-10 00:43:32 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char *file_name = __FILE__;
+
+	printf("%s\n", file_name);
+
+	return (0);
+}
